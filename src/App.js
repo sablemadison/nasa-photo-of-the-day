fc0f19af0a9159d73420f4dfddc35afc1ce2e404
Sable <sablechilds@yahoo.com>
2020-02-06 21:46:54 -0500

    added-component-file-structure-and-api-call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+
+  const [photo, setPhoto] = useState('');
+
+  
+
+  useEffect(()=> {
+    axiosData
+    .then(dataObj => {
+      console.log('data here:', dataObj)
+      console.log('url here', dataObj.data.url)
+      setPhoto(dataObj.data.url)
+      console.log('photo url:', photo)
+    })
+    .catch(error => {
+      console.log('error message:', error)
+    })
+  }, [])
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        
       </p>
     </div>
   );
 }
 
 export default App;
+
+const axiosData = axios.get('https://api.nasa.gov/planetary/apod?api_key=Mk4UTzOR5jVQvS8KvqaqC3v9fiZngJdzqKPf28ve')
